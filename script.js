29b36e019de2b9dedfe91aa8aaaacdba15ce0731
Larissa Mattos de Almeida Hora <CIANDT\larissah@lnb035650cps.ciandt.global>
2023-09-06 22:34:17 -0300

    creating calculator

---
+++ script.js
@@ -0,0 +1,23 @@
+const display = document.querySelector('.display')
+const buttons = document.querySelectorAll('button')
+const specialChars = ['%', '*', '/', '-', '+', '=']
+let output = ''
+
+const calculate = (buttonValue) => {
+  if (buttonValue === '=' && output !== '') {
+    output = eval(output.replace('%', '/100'))
+  } else if (buttonValue === 'AC') {
+    output = ''
+  } else if (buttonValue === 'DEL') {
+    output = output.toString().slice(0, -1)
+  } else {
+    if (output === '' && specialChars.includes(buttonValue)) return
+    output += buttonValue
+  }
+
+  display.value = output
+}
+
+buttons.forEach((button) => {
+  button.addEventListener('click', (e) => calculate(e.target.dataset.value))
+})
